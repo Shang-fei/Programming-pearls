d9ce8443574c668c1818b7262c3f1dd871f967e7
Shang fei <110293057+Shang-fei@users.noreply.github.com>
2023-07-25 12:32:22 +0800

    commit

--- chapter8/solution.c
+++ chapter8/solution.c
@@ -21,7 +21,7 @@ float fasb(float x)
 {
     return x>0 ? x : -x; 
 }
-/*基础扫描算法*/
+//9：
 float maxsum(float arr[], int n)
 {
     float maxsofar = *arr;
@@ -34,7 +34,7 @@ float maxsum(float arr[], int n)
     return maxsofar;
 }
 
-/*最接近t的子向量*/
+//10: 
 float findclosesubarr(float arr[], int n, float t)
 {
     float realarr[n];
@@ -56,6 +56,27 @@ float findclosesubarr(float arr[], int n, float t)
             min = abs_diff;
     }
     return min;
+}
+//12:
+
+//14:
+int closestSubarraySum(int n, float arr[], int m) {
+    float realarr[n];
+    float *cumarr = realarr + 1;
+    cumarr[-1] = 0;
+    for (int i = 0; i < n; i++) {
+        cumarr[i] = cumarr[i-1] + x[i];
+    }
+    int min_i = 0;
+    float min = INT_MAX;
+    for (int i = 0; i < n-m; i++) {
+        float abs_diff = fasb(cumarr[i+m] - cumarr[i-1]);
+        if (abs_diff < min) {
+            min= abs_diff;
+            min_i = i;
+        }
+    }
+    return min_i;
 }
 int main()
 {
